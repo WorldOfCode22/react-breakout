4f3ad9651bdcb4a29ffe300e57d46718df397411
Sloan <gwaltneys2@winthrop.edu>
2018-05-16 23:16:27 -0400

    env values are now directly from the enviorment class inside application

--- src/classes/application.ts
+++ src/classes/application.ts
@@ -1,21 +1,31 @@
 // logic class layer that is the top level of all API application functions
 import express = require("express");
 import {Environment} from "./environment";
+import {connect} from "mongoose";
 /**
  * Top level class of API logic
  */
 export class Application {
   private _expressServer: express.Application;
-  port: number;
+  _environment: Environment;
   constructor(environment: Environment) {
     this._expressServer = express();
-    this.port = environment.port;
+    this._environment = environment;
+    this.configServer();
     this.startServer();
   }
 
+  configServer(): void {
+    connect(this._environment.databaseURI)
+      .then(
+        () => {console.log("Database Connected");},
+        err => {console.log(err);}
+      );
+  }
+
   startServer():void {
-    this._expressServer.listen(this.port, () => {
-      console.log("Application active on port: " + this.port);
+    this._expressServer.listen(this._environment.port, () => {
+      console.log("Application active on port: " + this._environment.port);
     });
   }
 }
